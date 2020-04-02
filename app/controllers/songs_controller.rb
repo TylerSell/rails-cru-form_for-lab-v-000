a57ca1acc25bfccf57f984b8bfcfeb0fec55a01a
Tyler Sell <tylerselldesign@gmail.com>
2020-04-02 02:50:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,10 +12,14 @@ class SongsController < ApplicationController
     @song = Song.create(song_params)
     redirect_to song_path(@song)
   end
+  
+  def edit 
+    @song = Song.find(params[:id])
+  end 
    
   def update
     song = Song.update(song_params)
-    redirect_to song_path(@song)
+    redirect_to song_path(song)
   end
   
   def show 
